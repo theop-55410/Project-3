fcd2194525fdc2cddf96872f5bda26672e550ce2
Theo <theoprentice14@gmail.com>
2022-07-23 14:27:43 -0700

    enabled claimtokens and claimnft

--- game_files/scripts/blockchain.js
+++ game_files/scripts/blockchain.js
@@ -11,7 +11,7 @@ async function connectMetamask() {
     console.log("account's balance in ether:", balance.toString() / convertToEth);
 }
 
-/*async function claimTokens() {
+async function claimTokens() {
     const runTokenContractAddress = "0x00bcD49DcEaDbB4b9c7fF9E54a64CF2ad61ead61";
     const runTokenContractAbi = [
         "function mintTokens(address account, uint256 amount) public",
@@ -20,13 +20,13 @@ async function connectMetamask() {
     let convertToWei = 1000000000
     let amountToClaim = window.totalGweiScore * convertToWei
     await runTokenContract.connect(signer).mintTokens(signer.getAddress(), amountToClaim.toString())
-}*/
+}
 
-/*async function claimNft() {
+async function claimNft() {
     const nftContractAddress = "0x093298F529abdfCdA505e5C80A896478Dc32841a";
     const mintContractAbi = [
         "function mint(uint256 amount) public",
     ];
     const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
     await nftContract.connect(signer).mint(window.totalNFTScore.toString())
-}*/
+}
