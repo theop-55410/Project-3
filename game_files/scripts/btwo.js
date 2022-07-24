650c1d073e4414056b04747714c73b2409f8ee86
Theo <theoprentice14@gmail.com>
2022-07-24 14:54:06 -0700

    change

--- game_files/scripts/btwo.js
+++ game_files/scripts/btwo.js
@@ -4,6 +4,58 @@ import indexFile from "./index";
 let provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer;
 const treasury = "0x9DaC9f4B5323cB723464Cb52ADc7E7855BeB6294"
+const gameId = 1
+async function connectMetamask() {
+    await provider.send("eth_requestAccounts", []);
+    signer = await provider.getSigner();
+    console.log("Account address:", await signer.getAddress());
+    const balance = await signer.getBalance()
+    const convertToEth = 1e18;
+    console.log("account's balance in ether:", balance.toString() / convertToEth);
+}
+//onload
+async function startGame() {
+    signer = await provider.getSigner();
+    const rungameEscrowAddress = "0x360767822aCE73dceAdf51C2bb8256a4831A971d";
+    const rungameEscrowAbi = [
+        "function startGame(uint256 _gameId, address _treasury, uint256 _amount) external returns (uint256)", //might not need returns (bool)
+    ];
+    const runEscrowContract = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
+    await runEscrowContract.connect(signer).startGame(gameId, treasury, "1000000000000000000") //need a gameId function created to store the game instance in js, 1 is in there for test. Also need a function for player to enter amount they want to spend.
+    gameId = gameId++
+}
+//probably shouldn't be async
+async function playerWon() {
+    signer = await provider.getSigner();
+    const rungameEscrowAddress = "0x360767822aCE73dceAdf51C2bb8256a4831A971d";
+    const rungameEscrowAbi = [
+        "function playerWon(uint256 _gameId, address _player) external returns (bool)", //might not need returns (bool)
+    ];
+    const runEscrowContract = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
+    //let convertToWei = 1000000000
+    //let amountToClaim = window.totalGweiScore * convertToWei
+    await runEscrowContract.connect(signer).playerWon(gameId, signer.getAddress()) //needs to be a gameId variable but have 1 for test
+}
+//probabaly shouldn't be async
+async function playerLost() {
+    signer = await provider.getSigner();
+    const rungameEscrowAddress = "0x360767822aCE73dceAdf51C2bb8256a4831A971d";
+    const rungameEscrowAbi = [
+        "function playerLost(uint256 _gameId, address _player) external returns (bool)", //might not need returns (bool)?
+    ];
+    const runEscrowContract = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
+    //let convertToWei = 1000000000
+    //let amountToClaim = window.totalGweiScore * convertToWei
+    await runEscrowContract.connect(signer)(signer).playerLost(gameId, signer.getAddress())
+
+}
+
+
+
+/*
+let provider = new ethers.providers.Web3Provider(window.ethereum);
+let signer;
+const treasury = "0x9DaC9f4B5323cB723464Cb52ADc7E7855BeB6294"
 
 async function connectMetamask() {
     await provider.send("eth_requestAccounts", []);
@@ -32,7 +84,7 @@ async function playerWon() {
     const rungameEscrowAbi = [
         "function playerWon(uint256 _gameId, address _player) external returns (bool)", //might not need returns (bool)
     ];
-    //const rungameEscrowAddress = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
+    const rungameEscrowAddress = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
     //let convertToWei = 1000000000
     //let amountToClaim = window.totalGweiScore * convertToWei
     await rungameEscrowAddress.connect(signer).playerWon(10, signer.getAddress()) //needs to be a gameId variable but have 1 for test
@@ -50,3 +102,6 @@ async function playerLost() {
     //let amountToClaim = window.totalGweiScore * convertToWei
     await rungameEscrowAddress.connect(signer).playerWon(10, signer.getAddress())
 }
+
+*/
+
