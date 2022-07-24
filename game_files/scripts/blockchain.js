cd9f5398362168395f5c12784c2708265c968922
Theo <theoprentice14@gmail.com>
2022-07-24 13:47:23 -0700

    should have bugs removed

--- game_files/scripts/blockchain.js
+++ game_files/scripts/blockchain.js
@@ -30,3 +30,12 @@ async function claimNft() {
     const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
     await nftContract.connect(signer).mint(window.totalNFTScore.toString())
 }
+
+async function startGame() {
+    //const rungameEscrowAddress = "0x360767822aCE73dceAdf51C2bb8256a4831A971d";
+    //const rungameEscrowAbi = [
+        "function startGame(uint256 _gameId, address _treasury, uint256 _amount) external returns (uint256)", //might not need returns (bool)
+    //];
+    //const rungameEscrowAddress = new ethers.Contract(rungameEscrowAddress, rungameEscrowAbi, provider);
+    await rungameEscrowAddress.connect(signer).startGame(10, treasury, 1000000000000000000) //need a gameId function created to store the game instance in js, 1 is in there for test. Also need a function for player to enter amount they want to spend.
+}
